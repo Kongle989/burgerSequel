2d478a2d6c320069a6ede2c41162cc8bacaf3bce
Kong Le <Kong Le>
2017-05-11 16:13:30 -0400

    find and create eater

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,9 +4,15 @@ var db = require('../models'),
 
 router.get('/', function (req, res) {
     db.Burger.findAll({
-        order: '`burger_name` ASC'
+            order: '`burger_name` ASC'
 
-    }).then(function (dbBurger) {
+        }
+        // , {
+        //     include: {
+        //         model: db.Eater
+        //     }
+        // }
+    ).then(function (dbBurger) {
         var hbsOb = {
             burgers: dbBurger
         };
@@ -24,18 +30,25 @@ router.post("/", function (req, res) {
 });
 
 router.put("/:id", function (req, res) {
-    db.Burger.update(
-        {
-            devoured: req.body.devoured
-        },
-        {
-            where: {
-                id: req.params.id
-            }
-        })
-        .then(function (dbBurger) {
+
+    db.Eater.findOrCreate({
+        where: {
+            name: req.body.eater
+        }
+    }).then(function (dbEater) {
+        db.Burger.update(
+            {
+                devoured: req.body.devoured,
+                EaterId: dbEater[0].dataValues.id
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }).then(function (dbBurger) {
             res.redirect('/');
-        });
+        })
+    });
 });
 
 module.exports = router;
