13c9ca4ef7b1b2498024c4968f0bcf2cd12a9f31
Kong Le <Kong Le>
2017-05-11 13:53:43 -0400

    minor changes

--- models/burger.js
+++ models/burger.js
@@ -24,7 +24,5 @@ module.exports = function (sequelize, DataTypes) {
             }
         }
     );
-
     return Burger;
-
 };
--- models/eater.js
+++ models/eater.js
@@ -1,13 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
     var Eater = sequelize.define("Eater", {
-            // Giving the Author model a name of type STRING
             name: DataTypes.STRING
         },
         {
             classMethods: {
                 associate: function (models) {
-                    // Associating Author with Posts
-                    // When an Author is deleted, also delete any associated Posts
                     Eater.hasMany(models.Burger, {
                         onDelete: "cascade"
                     });
